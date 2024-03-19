a2d66f244b306cd714f26807c3f176f6667f6e75
TRD1611 <trimbakarao.despande@unikulinc.com>
2024-03-19 14:53:09 +0530

    Update on authorization changes 19-03-2024

--- Fee-GeneralPaymentList.aspx.cs
+++ Fee-GeneralPaymentList.aspx.cs
@@ -25,7 +25,7 @@ namespace HRMS
                 {
                     var role = lstUserRole.FirstOrDefault(x =>
                     string.Equals(x.User_Name, Helper.UserName, StringComparison.OrdinalIgnoreCase) &&
-                    string.Equals(x.Page_Name.Trim(), "Fee General Payment List", StringComparison.OrdinalIgnoreCase) &&
+                    string.Equals(x.Page_Name.Trim(), "General Payment List", StringComparison.OrdinalIgnoreCase) &&
                     string.Equals(x.Module_Name.Trim(), "Accounts", StringComparison.OrdinalIgnoreCase));
 
                     if (role == null || Convert.ToBoolean(role.Read))
@@ -55,7 +55,7 @@ namespace HRMS
                 {
                     var role = lstUserRole.FirstOrDefault(x =>
                     string.Equals(x.User_Name, Helper.UserName, StringComparison.OrdinalIgnoreCase) &&
-                    string.Equals(x.Page_Name.Trim(), "Fee General Payment List", StringComparison.OrdinalIgnoreCase) &&
+                    string.Equals(x.Page_Name.Trim(), "General Payment List", StringComparison.OrdinalIgnoreCase) &&
                     string.Equals(x.Module_Name.Trim(), "Accounts", StringComparison.OrdinalIgnoreCase));
 
                     if (role == null || Convert.ToBoolean(role.Insert))
