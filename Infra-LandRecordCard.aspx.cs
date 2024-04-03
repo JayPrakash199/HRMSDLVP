26c6532beb33544a27604a963d6e82de202a73ad
TRD1611 <trimbakarao.despande@unikulinc.com>
2024-04-03 17:49:19 +0530

    Authorization Changes for Infra 03-04-2024

--- Infra-LandRecordCard.aspx.cs
+++ Infra-LandRecordCard.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using HRMS.Common;
 using InfrastructureManagement.Common;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.UI.WebControls;
 using WebServices;
@@ -19,24 +20,52 @@ namespace HRMS
                     ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert(\"" + message + "\");", true);
                     Response.Redirect("Default.aspx");
                 }
-                ddlDistrict.DataSource = ODataServices.Getdistricts(Session["SessionCompanyName"] as string);
-                ddlDistrict.DataTextField = "Name";
-                ddlDistrict.DataValueField = "Code";
-                ddlDistrict.DataBind();
-                ddlDistrict.Items.Insert(0, new ListItem("Select District", "0"));
+
+                List<HRMSODATA.UserAuthorizationList> lstUserRole = ODataServices.GetUserAuthorizationList();
+                if (lstUserRole != null)
+                {
+                    var role = lstUserRole.FirstOrDefault(x =>
+                    string.Equals(x.User_Name, Helper.UserName, StringComparison.OrdinalIgnoreCase) &&
+                    string.Equals(x.Page_Name.Trim(), "Update Land Record", StringComparison.OrdinalIgnoreCase) &&
+                    string.Equals(x.Module_Name.Trim(), "Infra", StringComparison.OrdinalIgnoreCase));
+
+                    if (role == null || Convert.ToBoolean(role.Read))
+                    {
+                        ddlDistrict.DataSource = ODataServices.Getdistricts(Session["SessionCompanyName"] as string);
+                        ddlDistrict.DataTextField = "Name";
+                        ddlDistrict.DataValueField = "Code";
+                        ddlDistrict.DataBind();
+                        ddlDistrict.Items.Insert(0, new ListItem("Select District", "0"));
+                    }
+                    else
+                    {
+                        Alert.ShowAlert(this, "W", "You do not have permission to Read the content. Kindly contact the system administrator.");
+                    }
+                }
             }
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-            var obj = new WebServices.InfraLandCardReference.LandCard
-            {
-                Land_KisamSpecified = true,
-                Dispute_AreaSpecified = true,
-                Encroachment_Plot_AreaSpecified = true,
 
-                Khatian_Serial_No = txtKhatian_Serial_No.Text,
-                Land_Kisam = ddlLandKisam.SelectedValue == "Abadi_Irrigated_Two_Crops" ? WebServices.InfraLandCardReference.Land_Kisam.Abadi_Irrigated_Two_Crops
+            List<HRMSODATA.UserAuthorizationList> lstUserRole = ODataServices.GetUserAuthorizationList();
+            if (lstUserRole != null)
+            {
+                var role = lstUserRole.FirstOrDefault(x =>
+                string.Equals(x.User_Name, Helper.UserName, StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(x.Page_Name.Trim(), "Update Land Record", StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(x.Module_Name.Trim(), "Infra", StringComparison.OrdinalIgnoreCase));
+
+                if (role == null || Convert.ToBoolean(role.Insert))
+                {
+                    var obj = new WebServices.InfraLandCardReference.LandCard
+                    {
+                        Land_KisamSpecified = true,
+                        Dispute_AreaSpecified = true,
+                        Encroachment_Plot_AreaSpecified = true,
+
+                        Khatian_Serial_No = txtKhatian_Serial_No.Text,
+                        Land_Kisam = ddlLandKisam.SelectedValue == "Abadi_Irrigated_Two_Crops" ? WebServices.InfraLandCardReference.Land_Kisam.Abadi_Irrigated_Two_Crops
                 : ddlLandKisam.SelectedValue == "Abadi_Irrigated_One_crop" ? WebServices.InfraLandCardReference.Land_Kisam.Abadi_Irrigated_One_crop
                 : ddlLandKisam.SelectedValue == "Abadi_Non_irrigated_Rainfed" ? WebServices.InfraLandCardReference.Land_Kisam.Abadi_Non_irrigated_Rainfed
                 : ddlLandKisam.SelectedValue == "Abadi_Orchards_Bagayat" ? WebServices.InfraLandCardReference.Land_Kisam.Abadi_Orchards_Bagayat
@@ -52,88 +81,111 @@ namespace HRMS
                 : ddlLandKisam.SelectedValue == "Rakhit" ? WebServices.InfraLandCardReference.Land_Kisam.Rakhit
                 : ddlLandKisam.SelectedValue == "Sarbasadharana" ? WebServices.InfraLandCardReference.Land_Kisam.Sarbasadharana
                 : WebServices.InfraLandCardReference.Land_Kisam._blank_,
-                Plot_No = txtPlot_No.Text,
-                District = ddlDistrict.SelectedItem.Text,
-                Tahasil = txtTahasil.Text,
-                Village = txtVillage.Text,
-                RI_Circle = txtRI_Circle.Text,
-                Land_Owner_Details = txtLand_Owner_Details.Text,
-                Land_possessioner_Details = txtLand_possessioner_Details.Text,
-                Land_Issue_Description = txtLand_Issue_Description.Text,
-                Encroachment_Plot_Area = NumericHandler.ConvertToDecimal(txtEncroachment_Plot_Area.Text),
-                Encroachment_Plot_No = txtEncroachment_Plot_No.Text,
-                Dispute_Plot_No = txtDispute_Plot_No.Text,
-                Dispute_Area = NumericHandler.ConvertToDecimal(txtDispute_Area.Text),
-                CasePlot_No = txtCasePlot_No.Text
-            };
-            var result = SOAPServices.UpdateLandRecord(obj, Session["SessionCompanyName"] as string);
-            if (result == ResultMessages.UpdateSuccessfullMessage)
-            {
-                Alert.ShowAlert(this, "s", result);
-            }
-            else
-            {
-                Alert.ShowAlert(this, "e", result);
+                        Plot_No = txtPlot_No.Text,
+                        District = ddlDistrict.SelectedItem.Text,
+                        Tahasil = txtTahasil.Text,
+                        Village = txtVillage.Text,
+                        RI_Circle = txtRI_Circle.Text,
+                        Land_Owner_Details = txtLand_Owner_Details.Text,
+                        Land_possessioner_Details = txtLand_possessioner_Details.Text,
+                        Land_Issue_Description = txtLand_Issue_Description.Text,
+                        Encroachment_Plot_Area = NumericHandler.ConvertToDecimal(txtEncroachment_Plot_Area.Text),
+                        Encroachment_Plot_No = txtEncroachment_Plot_No.Text,
+                        Dispute_Plot_No = txtDispute_Plot_No.Text,
+                        Dispute_Area = NumericHandler.ConvertToDecimal(txtDispute_Area.Text),
+                        CasePlot_No = txtCasePlot_No.Text
+                    };
+                    var result = SOAPServices.UpdateLandRecord(obj, Session["SessionCompanyName"] as string);
+                    if (result == ResultMessages.UpdateSuccessfullMessage)
+                    {
+                        Alert.ShowAlert(this, "s", result);
+                    }
+                    else
+                    {
+                        Alert.ShowAlert(this, "e", result);
+                    }
+                }
+                else
+                {
+                    Alert.ShowAlert(this, "W", "You do not have permission to Submit the content. Kindly contact the system administrator.");
+                }
             }
         }
 
         protected void btnSearch_Click(object sender, EventArgs e)
         {
-            var result = ODataServices.GetLandDetailList(Session["SessionCompanyName"] as string);
-            var filteredResult = result.Where(x => x.Khatian_Serial_No == txtSearch.Text).FirstOrDefault();
-            if (filteredResult != null)
+
+            List<HRMSODATA.UserAuthorizationList> lstUserRole = ODataServices.GetUserAuthorizationList();
+            if (lstUserRole != null)
             {
-                txtKhatian_Serial_No.Text = filteredResult.Khatian_Serial_No;
-                ddlLandKisam.SelectedValue = Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Irrigated Two Crops" ? "Abadi_Irrigated_Two_Crops"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Irrigated One crop" ? "Abadi_Irrigated_One_crop"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Non irrigated (Rainfed)" ? "Abadi_Non_irrigated_Rainfed"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Orchards (Bagayat)" ? "Abadi_Orchards_Bagayat"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Water bodies (Jalashaya)" ? "Abadi_Water_bodies_Jalashaya"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Homestead (Gharabari)" ? "Abadi_Homestead_Gharabari"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Commercial (Byabasaika)" ? "Abadi_Commercial_Byabasaika"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Industrial (Shilpabhttika)" ? "Abadi_Industrial_Shilpabhttika"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Forest (Jungle)" ? "Abadi_Forest_Jungle"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Institutional (Anushthan)" ? "Abadi_Institutional_Anushthan"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Mine (Khani Khadan) & Others" ? "Abadi_Mine_Khani_Khadan__x0026__Others"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Abada Jogya Anabadi" ? "Abada_Jogya_Anabadi"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Abada Ajogya Anabadi" ? "Abada_Ajogya_Anabadi"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Rakhit" ? "Rakhit"
-                    : Convert.ToString(filteredResult.Land_Kisam) == "Sarbasadharana" ? "Sarbasadharana"
-                    : "";
-                txtPlot_No.Text = filteredResult.Plot_No;
-                ddlDistrict.SelectedItem.Text = filteredResult.District;
-                txtTahasil.Text = filteredResult.Tahasil;
-                txtVillage.Text = filteredResult.Village;
-                txtRI_Circle.Text = filteredResult.RI_Circle;
-                txtLand_Owner_Details.Text = filteredResult.Land_Owner_Details;
-                txtLand_possessioner_Details.Text = filteredResult.Land_possessioner_Details;
-                txtLand_Issue_Description.Text = filteredResult.Land_Issue_Description;
-                txtEncroachment_Plot_No.Text = filteredResult.Encroachment_Plot_No;
-                txtEncroachment_Plot_Area.Text = Convert.ToString(filteredResult.Encroachment_Plot_Area);
-                txtDispute_Plot_No.Text = filteredResult.Dispute_Plot_No;
-                txtDispute_Area.Text = Convert.ToString(filteredResult.Dispute_Area);
-                txtCasePlot_No.Text = filteredResult.CasePlot_No;
-                LblMessage.Text = string.Empty;
+                var role = lstUserRole.FirstOrDefault(x =>
+                string.Equals(x.User_Name, Helper.UserName, StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(x.Page_Name.Trim(), "Update Land Record", StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(x.Module_Name.Trim(), "Infra", StringComparison.OrdinalIgnoreCase));
+
+                if (role == null || Convert.ToBoolean(role.Read))
+                {
+                    var result = ODataServices.GetLandDetailList(Session["SessionCompanyName"] as string);
+                    var filteredResult = result.Where(x => x.Khatian_Serial_No == txtSearch.Text).FirstOrDefault();
+                    if (filteredResult != null)
+                    {
+                        txtKhatian_Serial_No.Text = filteredResult.Khatian_Serial_No;
+                        ddlLandKisam.SelectedValue = Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Irrigated Two Crops" ? "Abadi_Irrigated_Two_Crops"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Irrigated One crop" ? "Abadi_Irrigated_One_crop"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Non irrigated (Rainfed)" ? "Abadi_Non_irrigated_Rainfed"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Orchards (Bagayat)" ? "Abadi_Orchards_Bagayat"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Water bodies (Jalashaya)" ? "Abadi_Water_bodies_Jalashaya"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Homestead (Gharabari)" ? "Abadi_Homestead_Gharabari"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Commercial (Byabasaika)" ? "Abadi_Commercial_Byabasaika"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Industrial (Shilpabhttika)" ? "Abadi_Industrial_Shilpabhttika"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Forest (Jungle)" ? "Abadi_Forest_Jungle"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Institutional (Anushthan)" ? "Abadi_Institutional_Anushthan"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Abadi - Mine (Khani Khadan) & Others" ? "Abadi_Mine_Khani_Khadan__x0026__Others"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Abada Jogya Anabadi" ? "Abada_Jogya_Anabadi"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Abada Ajogya Anabadi" ? "Abada_Ajogya_Anabadi"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Rakhit" ? "Rakhit"
+                            : Convert.ToString(filteredResult.Land_Kisam) == "Sarbasadharana" ? "Sarbasadharana"
+                            : "";
+                        txtPlot_No.Text = filteredResult.Plot_No;
+                        ddlDistrict.SelectedItem.Text = filteredResult.District;
+                        txtTahasil.Text = filteredResult.Tahasil;
+                        txtVillage.Text = filteredResult.Village;
+                        txtRI_Circle.Text = filteredResult.RI_Circle;
+                        txtLand_Owner_Details.Text = filteredResult.Land_Owner_Details;
+                        txtLand_possessioner_Details.Text = filteredResult.Land_possessioner_Details;
+                        txtLand_Issue_Description.Text = filteredResult.Land_Issue_Description;
+                        txtEncroachment_Plot_No.Text = filteredResult.Encroachment_Plot_No;
+                        txtEncroachment_Plot_Area.Text = Convert.ToString(filteredResult.Encroachment_Plot_Area);
+                        txtDispute_Plot_No.Text = filteredResult.Dispute_Plot_No;
+                        txtDispute_Area.Text = Convert.ToString(filteredResult.Dispute_Area);
+                        txtCasePlot_No.Text = filteredResult.CasePlot_No;
+                        LblMessage.Text = string.Empty;
+                    }
+                    else
+                    {
+                        txtKhatian_Serial_No.Text = string.Empty;
+                        ddlLandKisam.SelectedValue = string.Empty;
+                        txtPlot_No.Text = string.Empty;
+                        ddlDistrict.SelectedItem.Text = string.Empty;
+                        txtTahasil.Text = string.Empty;
+                        txtVillage.Text = string.Empty;
+                        txtRI_Circle.Text = string.Empty;
+                        txtLand_Owner_Details.Text = string.Empty;
+                        txtLand_possessioner_Details.Text = string.Empty;
+                        txtLand_Issue_Description.Text = string.Empty;
+                        txtEncroachment_Plot_No.Text = string.Empty;
+                        txtEncroachment_Plot_Area.Text = string.Empty;
+                        txtDispute_Plot_No.Text = string.Empty;
+                        txtDispute_Area.Text = string.Empty;
+                        txtCasePlot_No.Text = string.Empty;
+                        LblMessage.Text = "No record found. Please try with valid Khatian Serial No.";
+                    }
+                }
+                else
+                {
+                    Alert.ShowAlert(this, "W", "You do not have permission to Search the content. Kindly contact the system administrator.");
+                }
             }
-            else
-            {
-                txtKhatian_Serial_No.Text = string.Empty;
-                ddlLandKisam.SelectedValue = string.Empty;
-                txtPlot_No.Text = string.Empty;
-                ddlDistrict.SelectedItem.Text = string.Empty;
-                txtTahasil.Text = string.Empty;
-                txtVillage.Text = string.Empty;
-                txtRI_Circle.Text = string.Empty;
-                txtLand_Owner_Details.Text = string.Empty;
-                txtLand_possessioner_Details.Text = string.Empty;
-                txtLand_Issue_Description.Text = string.Empty;
-                txtEncroachment_Plot_No.Text = string.Empty;
-                txtEncroachment_Plot_Area.Text = string.Empty;
-                txtDispute_Plot_No.Text = string.Empty;
-                txtDispute_Area.Text = string.Empty;
-                txtCasePlot_No.Text = string.Empty;
-                LblMessage.Text = "No record found. Please try with valid Khatian Serial No.";
             }
         }
     }
-}
--- Infra-MasterData.aspx.cs
+++ Infra-MasterData.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using HRMS.Common;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Net;
 using WebServices;
 
@@ -21,46 +23,78 @@ namespace HRMS
 
         protected void uploadBtn_Click(object sender, EventArgs e)
         {
-            if (this.csvUploader.HasFile)
+            List<HRMSODATA.UserAuthorizationList> lstUserRole = ODataServices.GetUserAuthorizationList();
+            if (lstUserRole != null)
             {
-                string fileExtention = Path.GetExtension(this.csvUploader.FileName);
-                string finalFileName = Path.GetFileNameWithoutExtension(this.csvUploader.FileName.Substring(0, 10)) + "_" + DateTime.Now.ToString("dd MMM yyyy") + fileExtention;
-                string path = Path.Combine(System.Web.HttpContext.Current.Server.MapPath("./" + "PDF" + "/"));
-                if (!Directory.Exists(path))
-                    Directory.CreateDirectory(path);
-                if (Directory.Exists(path))
+                var role = lstUserRole.FirstOrDefault(x =>
+                string.Equals(x.User_Name, Helper.UserName, StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(x.Page_Name.Trim(), "Land Master", StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(x.Module_Name.Trim(), "Infra", StringComparison.OrdinalIgnoreCase));
+
+                if (role == null || Convert.ToBoolean(role.Insert))
                 {
-                    path = Path.Combine(path, finalFileName);
-                    this.csvUploader.SaveAs(path);
+                    if (this.csvUploader.HasFile)
+                    {
+                        string fileExtention = Path.GetExtension(this.csvUploader.FileName);
+                        string finalFileName = Path.GetFileNameWithoutExtension(this.csvUploader.FileName.Substring(0, 10)) + "_" + DateTime.Now.ToString("dd MMM yyyy") + fileExtention;
+                        string path = Path.Combine(System.Web.HttpContext.Current.Server.MapPath("./" + "PDF" + "/"));
+                        if (!Directory.Exists(path))
+                            Directory.CreateDirectory(path);
+                        if (Directory.Exists(path))
+                        {
+                            path = Path.Combine(path, finalFileName);
+                            this.csvUploader.SaveAs(path);
+                        }
+                        string servicePath = ConfigurationManager.AppSettings["PdfPath"].ToString() + finalFileName;
+
+                        try
+                        {
+                            SOAPServices.ImportLandFile(servicePath, Session["SessionCompanyName"] as string);
+                        }
+                        catch (Exception ex)
+                        {
+                            string message = string.Format("Message: {0}\\n\\n", ex.Message);
+                            ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert(\"" + message + "\");", true);
+                        }
+                        Alert.ShowAlert(this, "s", "file uploaded successfully");
+                    }
                 }
-                string servicePath = ConfigurationManager.AppSettings["PdfPath"].ToString() + finalFileName;
-             
-                try
+                else
                 {
-                    SOAPServices.ImportLandFile(servicePath, Session["SessionCompanyName"] as string);
+                    Alert.ShowAlert(this, "W", "You do not have permission to Upload the content. Kindly contact the system administrator.");
                 }
-                catch (Exception ex)
-                {
-                    string message = string.Format("Message: {0}\\n\\n", ex.Message);
-                    ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert(\"" + message + "\");", true);
-                }
-                Alert.ShowAlert(this, "s", "file uploaded successfully");
             }
         }
 
         protected void downloadTemplateCSVBtn_Click(object sender, EventArgs e)
         {
-            string FileName = "LandDataExportTemplate.csv";
-            string exportedFilePath = ConfigurationManager.AppSettings["ExportFilePath"].ToString() + FileName;
-            WebClient wc = new WebClient();
-            byte[] buffer = wc.DownloadData(exportedFilePath);
+            List<HRMSODATA.UserAuthorizationList> lstUserRole = ODataServices.GetUserAuthorizationList();
+            if (lstUserRole != null)
+            {
+                var role = lstUserRole.FirstOrDefault(x =>
+               string.Equals(x.User_Name, Helper.UserName, StringComparison.OrdinalIgnoreCase) &&
+               string.Equals(x.Page_Name.Trim(), "Land Master", StringComparison.OrdinalIgnoreCase) &&
+               string.Equals(x.Module_Name.Trim(), "Infra", StringComparison.OrdinalIgnoreCase));
 
-            var fileName = "attachment; filename=" + FileName;
-            base.Response.ClearContent();
-            base.Response.AddHeader("content-disposition", fileName);
-            base.Response.ContentType = "application/vnd.ms-excel";
-            base.Response.BinaryWrite(buffer);
-            base.Response.End();
+                if (role == null || Convert.ToBoolean(role.Read))
+                {
+                    string FileName = "LandDataExportTemplate.csv";
+                    string exportedFilePath = ConfigurationManager.AppSettings["ExportFilePath"].ToString() + FileName;
+                    WebClient wc = new WebClient();
+                    byte[] buffer = wc.DownloadData(exportedFilePath);
+
+                    var fileName = "attachment; filename=" + FileName;
+                    base.Response.ClearContent();
+                    base.Response.AddHeader("content-disposition", fileName);
+                    base.Response.ContentType = "application/vnd.ms-excel";
+                    base.Response.BinaryWrite(buffer);
+                    base.Response.End();
+                }
+                else
+                {
+                    Alert.ShowAlert(this, "W", "You do not have permission to Download the content. Kindly contact the system administrator.");
+                }
+            }
         }
     }
 }
